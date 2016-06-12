dbca284adf561b37d50bbad94075c17e67dd333e
Glenn <glenn.recker@brittontechnologies.com>
2016-06-12 15:06:51 -0500

    pseudo code to fill JobModel

--- AFEWellBook/Controllers/OperatorController.cs
+++ AFEWellBook/Controllers/OperatorController.cs
@@ -11,9 +11,29 @@ namespace AFEWellBook.Controllers
         // GET: Operator
         public ActionResult Index()
         {
-
+			// GetJobs();
+			//get stuff to fill current job table
            
             return View();
         }
+        
+        
+        public ActionResult GetJobs()
+		{
+     		List<JobModel> jobList = new List<JobModel>();
+     
+	     	/*return (from r in results 
+			 select new jobList  {
+			     
+			     go to DB and fill the job model
+			 
+			 }).ToList()
+			  	*/
+			 
+		 
+     		return Json(jobList, JsonRequestBehavior.AllowGet);
+		}
+        
+        
     }
 }
